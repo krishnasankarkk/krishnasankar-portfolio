3d8ee650e9dbfcaba5a775480a4743fc0d327909
krishnasankar <sankarkrishna689@gmail.com>
2024-03-06 23:26:20 +0530

    design changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
                     <span className={`absolute ${isContactHovered ? 'block' : 'hidden'} bg-theme-1-3 -z-10 -top-2 -left-2 w-full h-10 rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom`}></span>
                 </span>
                 <span className="relative flex items-center p-4 text-md text-theme-1-1 cursor-pointer">Let&apos;s chat
-                <span className="absolute top-1 left-0 -z-10  bg-theme-1-3 w-24 h-12 rounded-tl-[85%] rounded-tr-[68%] rounded-bl-[76%] rounded-br-[53%]"></span></span>
+                <span className="absolute top-1 left-0 -z-10  bg-theme-1-4 border-2 border-theme-1-3 w-24 h-12 rounded-tl-[85%] rounded-tr-[68%] rounded-bl-[76%] rounded-br-[53%]"></span></span>
             </div>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,17 @@ function Projects() {
     return (
         <div className="w-full h-auto flex flex-col items-center justify-center rounded-2xl relative gap-2">
           <span className="font-extrabold text-2xl underline mt-4">Portfolio</span>
-          <div className="flex bg-theme-1-3 sm:w-4/5 md:w-4/5 h-auto rounded-2xl p-8 m-4">
-            <span className="font-bold">Project1</span>
+          <div className="flex gap-4 items-start justify-center bg-theme-1-4 border-2 border-theme-1-3 w-auto h-auto rounded-2xl p-8 m-4">
+            <img className="w-[40rem] h-auto rounded-2xl" src="/keralagram.png" alt="keralagram" />
+            <span className="text-xl font-bold"><a href="https://github.com/krishnasankarkk/django-keralagram" className="flex flex-row items-center justify-center text-2xl underline">Keralagram
+            <img src="/external-link.png" alt="link" /></a> <br />
+            A social network application crafted using django. <br />
+            Django is a full stack framework written in python.
+            </span>
           </div>
-          <div className="flex bg-theme-1-3 sm:w-4/5 md:w-4/5 h-auto rounded-2xl p-8 m-4"></div>
-          <div className="flex bg-theme-1-3 sm:w-4/5 md:w-4/5 h-auto rounded-2xl p-8 m-4"></div>
-          <div className="flex bg-theme-1-3 sm:w-4/5 md:w-4/5 h-auto rounded-2xl p-8 m-4"></div>
+          <div className="flex bg-theme-1-3 w-auto h-auto rounded-2xl p-8 m-4"></div>
+          <div className="flex bg-theme-1-3 w-auto h-auto rounded-2xl p-8 m-4"></div>
+          <div className="flex bg-theme-1-3 w-auto h-auto rounded-2xl p-8 m-4"></div>
         </div>
     )
 }
