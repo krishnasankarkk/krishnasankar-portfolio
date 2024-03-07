3790a4abf6a2d8aa7ad01f9d0b388d7463087c85
krishnasankar <sankarkrishna689@gmail.com>
2024-03-07 20:03:32 +0530

    fix some design bugs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,7 +41,7 @@ function Contact() {
     return (
         <div className="w-full flex xs:flex-col flex-wrap md:flex-row items-center justify-evenly p-4 gap-24 mt-24" id="contact">
           <div className="relative flex flex-col bg-theme-1-3 w-96 h-auto rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom">
-            <span className="w-[30rem] font-extrabold text-7xl z-50">Let&apos;s get <br /> in touch</span>
+            <span className="w-[30rem] font-extrabold text-7xl z-10">Let&apos;s get <br /> in touch</span>
             <span className="absolute -bottom-10 right-20 -z-0 flex flex-col bg-theme-1-4 opacity-65 border-2 border-theme-1-3 w-28 h-24 rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom"></span>
             <span className="font-bold text-4xl"><br />Say hello!</span>
             <span className="font-bold text-xl text-theme-1-2"><br />Phone</span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
                 <Link to="contact" className="relative cursor-pointer hidden md:block" spy={true} smooth={true} offset={-150} duration={500} onClick={() => { handleClick(); setContactClicked(true) }}  onMouseEnter={() => setContactHovered(true)} onMouseLeave={() => setContactHovered(false)}>Contact
                     <span className={`absolute ${isContactHovered | isContactClicked ? 'block' : 'hidden'} bg-theme-1-4 -z-10 -top-2 -left-2 w-full h-10 rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom`}></span>
                 </Link>
-                <a href="https://api.whatsapp.com/send?phone=917306310132" className="flex items-center p-4 text-md text-theme-1-1 cursor-pointer bg-theme-1-3 hover:bg-theme-1-4 border-2 border-theme-1-1 hover:border-theme-1-1 w-32 h-12 rounded-tl-[85%] rounded-tr-[68%] rounded-bl-[76%] rounded-br-[53%]">Let&apos;s chat</a>
+                <a href="https://api.whatsapp.com/send?phone=917306310132" className="flex items-center p-4 text-md text-theme-1-1 cursor-pointer bg-theme-1-3 hover:bg-theme-1-4 border-2 border-theme-1-1 hover:border-theme-1-1 w-40 justify-center h-12 rounded-tl-[85%] rounded-tr-[68%] rounded-bl-[76%] rounded-br-[53%]">Let&apos;s chat</a>
             </div>
         </div>
     )
