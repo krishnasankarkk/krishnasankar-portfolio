33f7c5601d1a0c66c35cfb1569cb541f88568703
krishnasankar <sankarkrishna689@gmail.com>
2024-03-08 20:42:56 +0530

    Loading screen

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ function Contact() {
   const [message, setMessage] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const form = useRef();
 
   const handleMessageChange = (e) => {
@@ -16,13 +18,15 @@ function Contact() {
     setEmail(e.target.value);
   };
 
-  const handlesetUsernameChange = (e) => {
+  const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
 
+  // Function to handle form submit.
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setIsLoading(true);
       await emailjs.sendForm(
         'service_bo945fs',
         'template_l2bxd28',
@@ -31,6 +35,11 @@ function Contact() {
           publicKey:'uWLEwtRUKNDc5vowO',
         }
       );
+      setIsLoading(false);
+      setIsVisible(true);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 2000);
     } catch (error) {
       console.error('Error sending email:', error);
     }
@@ -40,6 +49,20 @@ function Contact() {
 
     return (
         <div className="w-full flex flex-col lg:flex-row items-center justify-evenly p-4 gap-24 mt-24" id="contact">
+          {isVisible && (
+            <div className="fixed top-32 left-1/2 z-50 flex flex-row items-center justify-center bg-theme-1-4 border-2 border-theme-1-1 rounded-full text-theme-1-1 font-bold p-4 -translate-x-1/2">
+              <img src="/check.png" alt="Tick" />
+              <span>Message sent successfully!</span>
+            </div>
+          )}
+            {isLoading ? (
+              <div className="flex font-bold text-xl animate-spin fixed top-1/2 left-1/2">
+                <img className="w-14 h-14" src="/loading.png" alt="Loading" />
+              </div>
+            ) : (
+              <></>
+            )}
+          
           <div className="relative flex flex-col bg-theme-1-3 w-full h-auto rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom">
             <span className="font-extrabold text-5xl xs:text-7xl z-10">Let&apos;s get <br /> in touch</span>
             <span className="absolute -bottom-10 right-20 -z-0 flex flex-col bg-theme-1-4 opacity-65 border-2 border-theme-1-3 w-28 h-24 rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom"></span>
@@ -54,35 +77,38 @@ function Contact() {
             <div className="flex flex-col text-2xl w-full items-start">
               <span className="font-bold">I&apos;m excited to hear from you.</span>
               <span className="font-bold">Let&apos;s start something special together.</span>
-              <span className="font-bold">Call me for any query.</span>
+              <span className="font-bold">Contact me for any query.</span>
             </div>
             <div className="flex flex-col xs:flex-row text-xl w-full items-start gap-4 mt-10">
-              <input className="bg-transparent border-4 border-theme-1-3 p-4 rounded-2xl w-full" 
+              <input className="bg-transparent border-2 border-theme-1-3 p-4 rounded-2xl w-full" 
                 type="text" 
                 name="user_name" 
                 id="user_name" 
                 value={username}
-                onChange={handlesetUsernameChange}
+                required
+                onChange={handleUsernameChange}
                 placeholder="Name"/>
-              <input className="bg-transparent border-4 border-theme-1-3 p-4 rounded-2xl w-full" 
+              <input className="bg-transparent border-2 border-theme-1-3 p-4 rounded-2xl w-full" 
                 type="email" 
                 name="user_email" 
                 id="user_email" 
                 value={email}
+                required
                 onChange={handleEmailChange}
                 placeholder="Email"/>
             </div>
             <div className="flex xs:flex-col md:flex-row text-xl w-full items-start gap-4">
-              <textarea className="bg-transparent border-4 border-theme-1-3 p-4 rounded-2xl w-full h-52 items-start justify-start" 
+              <textarea className="bg-transparent border-2 border-theme-1-3 p-4 rounded-2xl w-full h-52 items-start justify-start" 
                 type="text" 
                 name="message" 
                 id="message" 
                 value={message}
+                required
                 onChange={handleMessageChange}
                 rows={8}
                 placeholder="Message"/>
             </div>
-            <button className="flex w-full h-20 bg-theme-1-4 border-4 border-theme-1-1 hover:bg-theme-1-3 items-center justify-center text-2xl font-bold rounded-2xl">Send</button>
+            <button className="flex w-full h-20 bg-theme-1-4 border-2 border-theme-1-1 hover:bg-theme-1-3 items-center justify-center text-2xl font-bold rounded-2xl">Send</button>
           </form>
         </div>
     )
