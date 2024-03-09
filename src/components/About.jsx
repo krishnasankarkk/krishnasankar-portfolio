bd2c469173cfc31cb92c1cb60d68366bad501925
krishnasankar <sankarkrishna689@gmail.com>
2024-03-09 17:00:11 +0530

    change in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
           <div className="flex flex-col bg-theme-1-3 w-72 xs:w-96 sm:w-[40rem] h-52 rounded-tl-[55%] rounded-tr-[68%] rounded-bl-[56%] rounded-br-[43%] rounded-custom">
             <span className="w-full font-extrabold text-xl md:text-2xl lg:text-3xl">Hi, It&apos;s me <br />
             <b className="text-2xl md:text-4xl lg:text-5xl">Krishnasankar</b><br />
-            A passionate Python developer with a knack for solving complex problems and creating efficient solutions.<br />
+            A passionate web developer with a knack for solving complex problems and creating efficient solutions.<br />
             </span>
             <a href="https://api.whatsapp.com/send?phone=917306310132" className="hidden md:flex flex-row gap-1 w-60 h-auto items-center justify-center mt-24 bg-theme-1-4 border-2 border-theme-1-3 hover:border-theme-1-1 hover:bg-theme-1-3 p-4 rounded-tl-[45%] rounded-tr-[58%] rounded-bl-[46%] rounded-br-[43%] cursor-pointer">
               <img src="/whatsapp.png" alt="whatsapp" />
