18e581c7dfe5c19030051f4566bc999e86b044cc
Krishnasankar <sankarkrishna689@gmail.com>
2024-03-10 13:42:16 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
       },
       colors: {
         'theme-1': {
-          1: '#000',
+          1: '#04364A',
           2: '#176B87',
           3: '#64CCC5',
           4: '#DAFFFB',
